798678242478f3bee5bb16df1a2c3e89dac076d5
ohhdemgirls <ohhdemgirls@ymail.com>
2014-01-07 22:57:04 +0000

    Update simpleurl.sh

--- simpleurl.sh
+++ simpleurl.sh
@@ -2,15 +2,21 @@
 
 basehost="$1"
 tag="$2"
-wget="wget -q"
+wget="wget -nv"
 apikey=$(cat api.key)
 offset=0
+mkdir -p -- "$basehost"
 
-apiurl=$($wget -O - "http://api.tumblr.com/v2/blog/$basehost/posts?api_key=$apikey&offset=$offset&tag=$tag")
-grep -q ',"posts":\[{' <<<"$apiurl" || break
-./json.sh <<<"$apiurl" | 
-grep -e '"original_size","url"]' -e ',"body"]' | 
-sed 's.\\..g' | 
-grep -Eo 'http://[^" ]+(gif|jpeg|jpg|png)' |
-sort -u > "$basehost.txt"; clear 
-echo "[+] URL's Written to $basehost.txt"
+while :
+do
+    apiurl=$($wget -O - "http://api.tumblr.com/v2/blog/$basehost/posts?api_key=$apikey&offset=$offset&tag=$tag")
+    grep -q ',"posts":\[{' <<<"$apiurl" || break
+
+    ./json.sh <<<"$apiurl" | 
+    grep -e '"original_size","url"]' -e ',"body"]' | 
+    sed 's.\\..g' | 
+    grep -Eo 'http://[^" ]+(gif|jpeg|jpg|png)' |
+    sort -u |
+    $wget -P "$basehost" -c -i - 
+    offset=$((offset+20))
+done
